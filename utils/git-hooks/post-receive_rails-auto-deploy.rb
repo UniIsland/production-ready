a062fd878aad77242ba5c34f272bcdb10bed532e
Tao <tao@huangtao.me>
2012-02-04 19:09:49 +0800

    determine which tasks to run before deploying

--- utils/git-hooks/post-receive_rails-auto-deploy.rb
+++ utils/git-hooks/post-receive_rails-auto-deploy.rb
@@ -1,21 +1,32 @@
 #!/usr/bin/env ruby
 
-branches = {
-    'refs/heads/master' => ['/home/www/cmusummit', 'origin', 'master'],
+deploy_to = {
+    'refs/heads/master' => ['/path/to/repo', 'origin', 'master', 'supervisor program'],
+    'refs/heads/live' => ['/path/to/repo', 'origin', 'live', 'supervisor program'],
 }
-logfile = 'path/to/logfile'
-deploy = []
+#logfile = 'path/to/logfile'
+
+old_dir = ENV['GIT_DIR']
+ENV['RAILS_ENV'] = 'production'
 
 STDIN.each do |line|
-    ref = line.split[2]
-    if branches.has_key?(ref)
-        deploy << ref
-    end
-end
+    oldrev, newrev, refname = line.split
+    next unless deploy_to.has_key?(refname)
 
-deploy.uniq.each do |ref|
-    target = branches[ref]
-    puts "deploying changes to branch '#{target[2]}':"
-    puts `export RAILS_ENV=production && cd #{target[0]} && unset GIT_DIR && git reset --hard && git pull #{target[1]} #{target[2]} && bundle install --deployment && bundle exec rake db:migrate && bundle exec rake assets:precompile && supervisorctl restart cmusummit`
+    ENV['GIT_DIR'] = old_dir
+    needs_db_migrate = ! `git diff --numstat #{oldrev} #{newrev} -- db/schema.rb`.empty?
+    needs_bundle_install = ! `git diff --numstat #{oldrev} #{newrev} -- Gemfile.lock`.empty?
+    needs_assets_precompile = ! `git diff --numstat #{oldrev} #{newrev} -- app/assets/`.empty?
+
+    dir, remote, branch, restart = deploy_to[refname]
+    puts "deploying branch '#{branch}' to '#{dir}':"
+    Dir.chdir(dir)
+    ENV['GIT_DIR'] = "#{dir}/.git"
+    puts `git reset --hard`
+    puts `git pull #{remote} #{branch}`
+    puts `bundle install --deployment` if needs_bundle_install
+    puts `bundle exec rake db:migrate` if needs_db_migrate
+    puts `bundle exec rake assets:precompile` if needs_assets_precompile
+    puts `supervisorctl restart #{restart}`
 end
 
