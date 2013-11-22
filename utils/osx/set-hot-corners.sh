cb523fac13c8cb943001e193162472910c7173f8
Tao <tao@huangtao.me>
2013-11-22 20:37:47 +0800

    add mac set-hot-corners.sh

---
+++ utils/osx/set-hot-corners.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+### com.apple.dock wvous-tl-corner
+### actions
+## disabled: 1,1048576
+## Mission Control: 2,0
+## Application Windows: 3,0
+## Desktop: 4,0
+## Dashboard: 7,0
+## Notifacation Center: 12,0
+## Launchpad: 11,0
+## Start Screen Saver: 5,0
+## Disable Screen Saver: 6,0
+## Put Display To Sleep: 10,0
+### com.apple.dock wvous-tl-modifier
+### modifiers
+## command: 1048576
+## shift: 131072
+## control: 262144
+## option: 524288
+
